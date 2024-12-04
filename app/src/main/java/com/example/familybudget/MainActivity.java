3fc2fa71dbc72df9ec758bca119a47e4f497d783
Neshizov <ethan.grand@bk.ru>
2024-12-04 18:51:13 +0500

    4 декабря, 2.5 недели до курсовой

--- app/src/main/java/com/example/familybudget/MainActivity.java
+++ app/src/main/java/com/example/familybudget/MainActivity.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
-    NavigationView navigationView;
     Button buttonViewCharts, buttonViewIncome, buttonViewSavings;
 
     @Override
@@ -19,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_view);
         buttonViewCharts = findViewById(R.id.buttonViewCharts);
         buttonViewIncome = findViewById(R.id.buttonViewIncome);
         buttonViewSavings = findViewById(R.id.buttonViewSavings);
@@ -30,17 +27,5 @@ public class MainActivity extends AppCompatActivity {
 
         buttonViewSavings.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SavingsActivity.class)));
 
-        navigationView.setNavigationItemSelectedListener(item -> {
-            int itemId = item.getItemId();
-            if (itemId == R.id.nav_income) {
-                startActivity(new Intent(MainActivity.this, IncomeActivity.class));
-            } else if (itemId == R.id.nav_expenses) {
-                startActivity(new Intent(MainActivity.this, ChartsActivity.class));
-            } else if (itemId == R.id.nav_savings) {
-                startActivity(new Intent(MainActivity.this, SavingsActivity.class));
-            }
-            drawerLayout.closeDrawers();
-            return true;
-        });
     }
 }
